1538fbd73b4bbf42146d856b62f8be3c350f1d5b
Karan Sutradhar <60763520+Karansutradhar@users.noreply.github.com>
2021-02-21 19:30:43 -0500

    Added the code file

---
+++ hw2.py
@@ -0,0 +1,31 @@
+# import the necessary packages
+import time     # import time
+import cv2
+from picamera.array import PiRGBArray       # import PiRGBArray from picamera.array as assign name to it
+from picamera import PiCamera       # import PiCamera from picamera as assign name to it
+
+
+# initialize the Raspberry Pi camera
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 25
+rawCapture = PiRGBArray(camera, size=(640,480))
+# define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*’XVID’)
+out = cv2.VideoWriter(‘videoname.avi’, fourcc, 10, (640, 480))
+# allow the camera to warmup
+time.sleep(0.1)
+# keep looping
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=False):
+  # grab the current frame
+  image = frame.array
+  # show the frame to our screen
+  cv2.imshow("Frame", image)
+  key = cv2.waitKey(1) & 0xFF
+  # clear the stream in preparation for the next frame
+  rawCapture.truncate(0)
+  # press the 'q' key to stop the video stream
+  if key == ord("q"):
+    break
+  # write frame to video file
+  out.write(image)
